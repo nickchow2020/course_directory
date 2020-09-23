157575a8ba0938f1686c266ecb03d471435267f7
Stephenchow <39304873+stephencow@users.noreply.github.com>
2020-09-22 21:26:34 -0700

    Updates

---
+++ precache-manifest.0eb84a86b3a95a83d22153214622e7ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a202041821e1e2a30bdc",
+    "url": "/course_directory/static/css/main.b844a23d.chunk.css"
+  },
+  {
+    "revision": "a202041821e1e2a30bdc",
+    "url": "/course_directory/static/js/main.a2020418.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/course_directory/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "64696d5a14bdb785a063",
+    "url": "/course_directory/static/js/runtime~main.64696d5a.js"
+  },
+  {
+    "revision": "3555c8064d74fdea9d8220b6a1aa9ef5",
+    "url": "/course_directory/index.html"
+  }
+];
